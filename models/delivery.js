35856daed2a3454150fb7dc506f99e33a9d32e97
vinchang <vinchang@mit.edu>
2016-11-20 04:01:27 -0500

    Data model template.

--- models/delivery.js
+++ models/delivery.js
@@ -2,18 +2,18 @@ var mongoose = require("mongoose");
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var DeliverySchema = mongoose.Schema({
-    stores: {type: String, required: true, index: true}, // username must be kerberos
-    status:
-    deadline:
-    itemName:
-    itemDescription:
-    itemPrice:
-    tips:
-    pickupLocation:
-    requester:
-    shopper:
-    pickupTime:
-});
+    stores: [{type: String, required: true}], // A list of possible grocery stores.
+    status: {type: String, required: true},
+    deadline: {type: Date, required: true},
+    itemName: {type: String, required: true},
+    itemDescription: {type: String, required: true},
+    itemPrice: {type: Number, required: true},
+    tips: {type: Number, required: true},
+    pickupLocation: {type: String, required: true},
+    requester: {type: ObjectId, ref: "User", required: true},
+    shopper: {type: ObjectId, ref: "User"},
+    pickupTime: Date
+}); 
 
 
 
