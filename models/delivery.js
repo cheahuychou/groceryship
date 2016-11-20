251f79feace4d7151bdca6b1d0227332e7470b6e
vinchang <vinchang@mit.edu>
2016-11-20 05:15:53 -0500

    Added two delivery model validations.

--- models/delivery.js
+++ models/delivery.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+var utils = require("../public/javascripts/utils.js")
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var DeliverySchema = mongoose.Schema({
@@ -7,14 +8,28 @@ var DeliverySchema = mongoose.Schema({
     deadline: {type: Date, required: true},
     itemName: {type: String, required: true},
     itemDescription: {type: String, required: true},
-    itemPrice: {type: Number, required: true},
+    itemQuantity: {type: Number, required: true},
+    estimatedPrice: {type: Number, required: true},
     tips: {type: Number, required: true},
     pickupLocation: {type: String, required: true},
     requester: {type: ObjectId, ref: "User", required: true},
     shopper: {type: ObjectId, ref: "User"},
+    actualPrice: Number,
     pickupTime: Date
 }); 
 
+DeliverySchema.path("stores").validate(function(stores) {
+		utils.each(stores, function(store) {
+			if (!(store == "hMart" || store == "starMarket" || store == "traderJoes" || store == "wholeFoods")){
+				return false
+			} 
+			return true
+		});
+}, "Not a valid grocery store");
+
+DeliverySchema.path("status").validate(function(status) {
+    return status == "pending" || status == "claimed" || status == "rejected" || status == "accepted";
+}, "Not a valid status");
 
 
 var DeliveryModel = mongoose.model("Delivery", DeliverySchema);
