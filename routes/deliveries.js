d78f251f36eff0e66e7a2564f2472fadc4a9af0b
Joseph Kuan <jsphkn@mit.edu>
2016-11-20 08:01:27 -0500

    minor changes

--- routes/deliveries.js
+++ routes/deliveries.js
@@ -3,7 +3,17 @@ var router = express.Router();
 var Delivery = require('../models/delivery');
 
 router.get("/", function(req, res){
-
+	var username = req.session.passport.user.username;
+	var userID //TODO
+	var 
+	Delivery.find({requester: userID})
+	        .exec(function(err, requestList) {
+	        	Delivery.find({shopper: userID})
+	        	        .exec(function(err, shopperList) {
+	        	        	
+	        	        })
+	        })
+    res.render('dashboard', { title: 'Dashboard', username: req.params.username});
 });
 
 router.post("/", function(req, res){
@@ -11,6 +21,26 @@ router.post("/", function(req, res){
 });
 
 router.get("/:id", function(req, res){
+
+});
+
+router.delete("/:id", function(req, res){
+
+});
+
+router.put("/:id/claim", function(req, res){
+
+});
+
+router.put("/:id/deliver", function(req, res){
+
+});
+
+router.put("/:id/accept", function(req, res){
+
+});
+
+router.put("/:id/reject", function(req, res){
 
 });
 
