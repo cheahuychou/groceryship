18149eb04dabd4ca0417215ec608f15fba5f0cb4
vinchang <vinchang@mit.edu>
2016-11-20 02:13:08 -0500

    Routing template.

---
+++ routes/deliveries.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var Delivery = require('../models/delivery');
+
+router.get("/", function(req, res){
+
+});
+
+router.post("/", function(req, res){
+
+});
+
+router.get("/:id", function(req, res){
+
+});
+
