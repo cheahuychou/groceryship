9cbff0b2835b27ee6091403aaeee9fb52361d271
Cheahuychou Mao <cmao18@mit.edu>
2016-11-20 21:02:05 -0500

    Fixed bugs for validating signup information

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,6 +6,7 @@
 *					the constraints 
 */
 var checkLogInForm = function () {
+	console.log('can you see me login');
 	var kerberos = $('#kerberos-box').val().trim();
 	var password = $('#password-box').val().trim();
 	if (kerberos.length === 0 || /<[a-z][\s\S]*>/i.test(kerberos)) {
@@ -18,6 +19,8 @@ var checkLogInForm = function () {
 	}
 }
 
+console.log('can you see me');
+
 /**
 * Checks that the fields in the signup form are non-empty strings,
 * valid and do not contain html elements, alert the user if
@@ -26,13 +29,14 @@ var checkLogInForm = function () {
 *					the constraints 
 */
 var checkSignUpForm = function () {
+	console.log('can you see me signup');
 	var kerberos = $('#kerberos-register-box').val().trim();
 	var password = $('#password-register-box').val().trim();
 	var confirmPassword = $('#confirm-password-register-box').val().trim();
 	var mitId = $('#mit-id-register-box').val().trim();
-	var phoneNumber = $('#phone_number-register-box').val().trim();
-	var dorm = $('#dorm-register-box').val().trim();
-	console.log(/<[a-z][\s\S]*>/i.test(kerberos), /<[a-z][\s\S]*>/i.test(password));
+	var phoneNumber = $('#phone-number-register-box').val().trim();
+	var dorm = $('.dorm :selected').text().trim();
+	console.log(kerberos, password, confirmPassword, mitId, phoneNumber, dorm);
 	if (kerberos.length === 0 || kerberos.toLowerCase() !== kerberos || /<[a-z][\s\S]*>/i.test(kerberos)) {
 		alert('Please enter a non-empty and valid kerberos');
 		return false; 
@@ -45,8 +49,12 @@ var checkSignUpForm = function () {
 		alert('The password and confirm password you entered did not match, please try again.');
 		return false;
 	}
-	if (mitId.match(/^\d+$/) || mitId.length != 9) {
+	if (!mitId.match(/^\d+$/) || mitId.length != 9) {
 		alert('MIT ID must be a nine-digit number');
+		return false;
+	}
+	if (!phoneNumber.match(/^\d+$/) || phoneNumber.length != 10) {
+		alert('US phone numbers must have 10 digits');
 		return false;
 	}
 
