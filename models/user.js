412cf8486a4843fbe400dd1c9d2767f877ebc4bb
Cheahuychou Mao <cmao18@mit.edu>
2016-11-22 02:12:30 -0500

    Add tests and more validation for User model

--- models/user.js
+++ models/user.js
@@ -17,6 +17,20 @@ UserSchema.path("password").validate(function(value) {
     return value.trim().length > 0;
 }, "No empty passwords");
 
+UserSchema.path("mitId").validate(function(value) {
+    return value.toString().length === 9;
+}, "MIT ID must have exactly 9 digits");
+
+UserSchema.path("phoneNumber").validate(function(value) {
+    return value.toString().length === 10;
+}, "US phone numbers must have exactly 10 digits");
+
+UserSchema.path("dorm").validate(function(value) {
+	var dorms = ['Maseeh', 'McCormick', 'Baker', 'Burton Cornor', 'MacGregor', 'New House', 'Next House',
+					'East Campus', 'Senior House']
+    return value.trim().length > 0 && dorms.indexOf(value) > -1;
+}, "MIT ID must have 9 digits");
+
 
 var UserModel = mongoose.model("User", UserSchema);
 
---
+++ test/user_test.js
@@ -0,0 +1,115 @@
+var assert = require('chai').assert;
+var mongoose = require("mongoose");
+var User = require("../models/user");
+
+describe("Models", function() {
+  // The mongoose connection object.
+  var con;
+  // Before running any test, connect to the database.
+  before(function(done) {
+    con = mongoose.connect("mongodb://localhost/grocery-test", function() {
+      done();
+    });
+  });
+
+  // Delete the database before each test.
+  beforeEach(function(done) {
+    con.connection.db.dropDatabase(function() { done(); });
+  });
+
+  describe("User", function() {
+
+    describe("Create", function() {
+      it("should create a user successfully when all fields are present and valid", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 123456789, "phoneNumber": 1234567890, "dorm": "Maseeh"}
+        User.create(userJSON, function(err, user) {
+          assert.isNull(err);
+          User.findOne({"username": "testuser1"}, function(err, user) {
+            done();
+          });
+        });
+      });
+
+      it("should fail to create a user when the username is missing", function(done) {
+        var userJSON = {"password": "123456", "mitId": 123456789, "phoneNumber": 1234567890, "dorm": "Maseeh"};
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should fail to create a user when the password is missing", function(done) {
+        var userJSON = {"username": "testuser", "mitId": 123456789, "phoneNumber": 1234567890, "dorm": "Maseeh"};
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should fail to create a user when the MIT ID is missing", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "phoneNumber": 1234567890, "dorm": "Maseeh"};
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should fail to create a user when the phone number is missing", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 123456789, "dorm": "Maseeh"};
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should fail to create a user when the dorm is missing", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 123456789, "dorm": "Maseeh"};
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should not allow empty usernames", function(done) {
+        var userJSON = {"username": "   ", "password": "123456", "mitId": 123456789, "phoneNumber": 1234567890, "dorm": "Maseeh"}
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should not allow empty passwords", function(done) {
+        var userJSON = {"username": "testuser", "password": "   ", "mitId": 123456789, "phoneNumber": 1234567890, "dorm": "Maseeh"}
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should not allow invalid MIT ID", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 12345678, "phoneNumber": 1234567890, "dorm": "Maseeh"}
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should not allow invalid phone number", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 123456789, "phoneNumber": 12345678, "dorm": "Maseeh"}
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+      it("should not allow invalid dorm", function(done) {
+        var userJSON = {"username": "testuser", "password": "123456", "mitId": 123456789, "phoneNumber": 12345678, "dorm": "WILG"}
+        User.create(userJSON, function(err, user) {
+          assert.isNotNull(err);
+          done();
+        });
+      });
+
+    });
+  });
+});
