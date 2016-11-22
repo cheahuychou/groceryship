5047a4e495c74a07f7f3b0b1bf3971f8d27478e8
mcslao <czarina.s.lao@gmail.com>
2016-11-22 01:48:58 -0500

    Add forgotten navbar javascript file

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.logout-button').click(function() {
+        $.post('/logout', {}, function() {
+            window.location.replace('/');
+        });
+    });
+});
