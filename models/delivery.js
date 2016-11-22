889dc2019b78241e8b77dbefcb848086fb8180b9
mcslao <czarina.s.lao@gmail.com>
2016-11-22 01:00:37 -0500

    Remove more unnecessary changes in model

--- models/delivery.js
+++ models/delivery.js
@@ -5,17 +5,17 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var DeliverySchema = mongoose.Schema({
     stores: [{type: String, required: true}], // A list of possible grocery stores.
     status: {type: String, required: true},
-    deadline: {type: String, required: true},
+    deadline: {type: Date, required: true},
     itemName: {type: String, required: true},
     itemDescription: {type: String, required: true},
-    itemQuantity: {type: Number, required: true},
+    itemQuantity: {type: String, required: true},
     estimatedPrice: {type: Number, required: true},
     tips: {type: Number, required: true},
     pickupLocation: {type: String, required: true},
     requester: {type: ObjectId, ref: "User", required: true},
     shopper: {type: ObjectId, ref: "User", default: null},
     actualPrice: {type: Number, default: null},
-    pickupTime: {type: String, default: null}
+    pickupTime: {type: Date, default: null}
 }); 
 
 DeliverySchema.path("stores").validate(function(stores) {
@@ -33,13 +33,9 @@ DeliverySchema.path("shopper").validate(function(shopperId) {
 	return shopperId != this.requester;
 }, "The shopper and the requester should not be the same.");
 
-DeliverySchema.path("pickupTime").validate(function(pickupTime) {
-    return (!pickupTime || new Date(this.deadline) >= new Date(pickupTime));
-}, "The dealine has passed.");
-
 DeliverySchema.path("deadline").validate(function(deadline) {
-	return new Date(deadline) >= new Date();
-}, "The dealine must be after the current time.");
+    return deadline >= this.pickupTime;
+ }, "The dealine has passed.");
 
 var DeliveryModel = mongoose.model("Delivery", DeliverySchema);
 
