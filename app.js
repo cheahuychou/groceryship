b2ff6c97b51c60cf7d5480f4b5fa5cae77367f7a
mcslao <czarina.s.lao@gmail.com>
2016-12-01 18:52:47 -0500

    Remove unnecessary concat helper

--- app.js
+++ app.js
@@ -35,9 +35,6 @@ app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'index', helpers: {if
     add: function(a, b) {
         return a+b;
     },
-    concat: function(a, b) {
-        return a+' '+b;
-    },
     ifContains: function(a, b, options) {
         if (a instanceof Array) {
             if (a.indexOf(b) > -1) {
