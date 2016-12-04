25a9b9d72add915c3479559ae6ab3aaccb9b48ac
mcslao <czarina.s.lao@gmail.com>
2016-12-04 16:09:19 -0500

    Update min shipping rating in request form and route

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
             var deadline = $('input[name=item-due]').val();
             var pickupLocation = $('select[name=item-pickup]').val();
             var tips = $('input[name=item-tip]').val();
-            var minimumShopperRating = $('select[name=minimum-shopper-rating').val();
+            var minShippingRating = $('select[name=minimum-shipping-rating').val();
             var description = $('input[name=item-description]').val();
             var csrf = $('#csrf').val();
             console.log(csrf);
@@ -80,7 +80,7 @@ $(document).ready(function() {
                 itemDescription: description,
                 itemTips: tips,
                 itemPickupLocation: pickupLocation,
-                minimumShopperRating: minimumShopperRating,
+                minShippingRating: minShippingRating,
                 _csrf: csrf
             }, function(data) {
                 if (!data.success) {
