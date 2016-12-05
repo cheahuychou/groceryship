3a075277eadb91589f76250a9205c046c9eb3909
vinchang <vinchang@mit.edu>
2016-12-05 01:25:50 -0500

    fix database connection bug

--- test/rating_test.js
+++ test/rating_test.js
@@ -6,53 +6,83 @@ var Delivery = require("../models/delivery");
 describe("App", function() {
 	// The mongoose connection object.
 	var con;
+
+	// Test users.
+	var testUser1, testUser2;
+
+	// Ids of the default test users.
+	var id1, id2;
+
+	// Some default deliveries.
+	var testDeliveryJSON;
+
 	// Before running any test, connect to the database.
 	before(function(done) {
 		con = mongoose.connect("mongodb://localhost/grocery-test", function() {
-			done();
+			con.connection.db.dropDatabase(function() { 
+		    	var user1 = new User({
+				    "username": "test-user1",
+					"password": "Iwantpizza3@",
+					"firstName": "firstName1",
+					"lastName": "lastName1",
+					"phoneNumber": 1234567890,
+					"dorm": "Baker",
+					"stripeId":"testuserStripeId", 
+					"stripeEmail": "testuserStripeEmail",
+					"stripePublishableKey": "testuserStripePublishableKey"
+				});
+
+				var user2 = new User({
+					"username": "test-user2",
+					"password": "Iwantpizza3@",
+					"firstName": "firstName2",
+					"lastName": "lastName2",
+					"phoneNumber": 2345678900,
+					"dorm": "Random",
+					"stripeId":"testuserStripeId", 
+					"stripeEmail": "testuserStripeEmail",
+					"stripePublishableKey": "testuserStripePublishableKey"
+				});
+
+				User.create([user1, user2], function(err, users) {
+					if (err) {
+					console.log("Default users not created");
+					console.log(err.message);
+					}
+
+					testUser1 = users[0];
+					testUser2 = users[1];
+
+					id1 = testUser1._id;
+					id2 = testUser2._id;
+
+					testDeliveryJSON = {
+					    stores: ["HMart"],
+				        status: "pending",
+				      	deadline: new Date('2016-11-21T23:59:59'),
+				      	itemName: "test-item",
+				        itemDescription: "test-description",
+				        itemQuantity: "test-quantity",
+				        estimatedPrice: 3.5,
+				        tips: 0.5,
+				        pickupLocation: "Baker",
+				        requester: id1,
+				      	shopper: id2
+				    }
+					done(); 
+				});
+		    });
 		});
 	});
 
-	// Delete the database before each test.
+	// Delete the deliveries collection before each test.
 	beforeEach(function(done) {
-	    con.connection.db.dropDatabase(function() { done(); });
+		con.connection.db.dropCollection("deliveries", function() { done(); });
 	});
 
 	describe("Rating", function() {
 
-		var testUser1 = new User({
-		    "username": "test-user1",
-			"password": "Iwantpizza3@",
-			"phone_number": 1,
-			"dorm": "Baker",
-			"stripeId":"testuserStripeId", 
-			"stripeEmail": "testuserStripeEmail"}
-		);
-		var id1 = testUser1._id;
-
-		var testUser2 = new User({
-			"username": "test-user2",
-			"password": "Iwantpizza3@",
-			"phone_number": 2,
-			"dorm": "Random",
-			"stripeId":"testuserStripeId", 
-			"stripeEmail": "testuserStripeEmail"}
-		);
-		var id2 = testUser2._id;
-
-		var testDeliveryJSON = {
-		    stores: ["HMart"],
-	        status: "pending",
-	      	deadline: new Date('2016-11-21T23:59:59'),
-	      	itemName: "test-item",
-	        itemDescription: "test-description",
-	        itemQuantity: "test-quantity",
-	        estimatedPrice: 3.5,
-	        tips: 0.5,
-	        pickupLocation: "Baker",
-	        requester: id1,
-	      	shopper: id2
-	    }
+		
 
 	  	it("can contain requester and shopper ratings in the delivery model", function(done) {
   			testDeliveryJSON["requesterRating"] = 4;
