33018218b61b457fc31b9fa3f108bb46cb17b8df
mcslao <czarina.s.lao@gmail.com>
2016-12-06 14:20:08 -0500

    Fix profile star ratings

--- public/javascripts/user_info.js
+++ public/javascripts/user_info.js
@@ -1,45 +1,53 @@
 // Author: Czarina Lao
-$(document).ready(function () { 
-    // Adapted from: https://github.com/FortAwesome/Font-Awesome/issues/4147
-    var fillStars = function(selector) {
-        var NUM_STARS = 5;
-        $(selector).each(function(i, e) {
-            var rating = parseFloat($(e).text());
-            var starSize = ($(e).width() / NUM_STARS);
 
-            $(e).html($('<span>', {
-                width: rating * starSize
-            }));
-        });
-    };
+var getContactTooltip = function() {
+    var targetElement = $(this).attr('data-target');
 
-    fillStars('.star-rating');
+    // only show the relevant rating: either the shipping or the request rating
+    var viewType = $(this).attr('data-view');
+    switch (viewType) {
+        case 'shopper':
+            $(targetElement + ' .shipping-rating').prop('hidden', false);
+            $(targetElement + ' .request-rating').prop('hidden', true);
+            break;
+        case 'request-feed':
+            $(targetElement + ' .shipping-rating').prop('hidden', false);
+            $(targetElement + ' .request-rating').prop('hidden', true);
+            $(targetElement + ' .user-phone').prop('hidden', true);
+            break;
+        case 'requester':
+            $(targetElement + ' .shipping-rating').prop('hidden', true);
+            $(targetElement + ' .request-rating').prop('hidden', false);
+            break;
+        default:
+            break;
+    }
 
-    var getContactTooltip = function() {
-        var targetElement = $(this).attr('data-target');
+    return $(targetElement).html();
+};
 
-        // only show the relevant rating: either the shipping or the request rating
-        var viewType = $(this).attr('data-view');
-        switch (viewType) {
-            case 'shopper':
-                $(targetElement + ' .shipping-rating').prop('hidden', false);
-                $(targetElement + ' .request-rating').prop('hidden', true);
-                break;
-            case 'request-feed':
-                $(targetElement + ' .shipping-rating').prop('hidden', false);
-                $(targetElement + ' .request-rating').prop('hidden', true);
-                $(targetElement + ' .user-phone').prop('hidden', true);
-                break;
-            case 'requester':
-                $(targetElement + ' .shipping-rating').prop('hidden', true);
-                $(targetElement + ' .request-rating').prop('hidden', false);
-                break;
-            default:
-                break;
-        }
+// Adapted from: https://github.com/FortAwesome/Font-Awesome/issues/4147
+var fillStars = function(selector, isTooltip) {
+    // need tooltip star size because css does weird things
+    // and we can't get the element's proper width when updating ratings
+    var TOOLTIP_STAR_SIZE = 13;
+    var NUM_STARS = 5;
+    $(selector).each(function() {
+        var rating = parseFloat($(this).text());
+        var starSize = isTooltip ? TOOLTIP_STAR_SIZE : ($(this).width() / NUM_STARS);
 
-        return $(targetElement).html();
-    };
+        $(this).html($('<span>', {
+            width: rating * starSize,
+        })).append($('<span>', {
+            class: 'sr-only',
+            text: 'Rating: ' + rating.toFixed(2) + ' out of ' + NUM_STARS
+        }));
+    });
+};
+
+$(document).ready(function () {
+    fillStars('.star-rating.tooltip-stars',true);
+    fillStars('.star-rating.profile-stars',false);
 
     $('[data-toggle="tooltip"]').tooltip({
         title: getContactTooltip,
